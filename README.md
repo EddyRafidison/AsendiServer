cd3592748840cba9d92334983abd6f3cc9c309b1
root <root@localhost.localdomain>
2025-09-16 23:41:46 +0200

    update

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # OneX
-
-## Features
-- Fast
-- Low transaction fee
-- Profit generator
-
-Based on [this idea](https://chatgpt.com/canvas/shared/67fa3b2e392c8191831400ea7591c4f1) and [this logic](https://drive.google.com/file/d/10GzY9u-b6Y9Vi7VdVlZxZYOkSssJKWgD/view?usp=drivesdk)
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = require("./routes");
+const { initDbIfEmpty, cleanup, PORT_SERVER } = require("./db");
+const { httpsRedirect } = require("./utils");
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(httpsRedirect);
+
+// on monte toutes les routes définies dans routes.js
+app.use("/", routes);
+
+// on démarre
+app.listen(PORT_SERVER, () => {
+  console.log(`Server on port ${PORT_SERVER}`);
+  initDbIfEmpty();
+  setInterval(cleanup, 10_800_000);
+});
